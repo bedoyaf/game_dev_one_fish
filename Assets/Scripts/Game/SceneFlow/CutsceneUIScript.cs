6b23daed13a74be49d09e0cf12097545142d7c15
Jakub Jankovec <jankovec.jakub@gmail.com>
2026-05-27 16:27:30 +0200

    Add cutscenes

---
+++ Assets/Scripts/Game/SceneFlow/CutsceneUIScript.cs
@@ -0,0 +1,100 @@
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+
+/// <summary>
+/// Starts playing a set of images in order after some delay.
+/// 
+/// After it is done playing, will start the game (hard coded)
+/// Also has a skip button feature, that will trigger the action immediately.
+/// 
+/// </summary>
+public class CutsceneUIScript : MonoBehaviour
+{
+
+    // How long before the cutscenes start playing
+    [SerializeField] private float startDelay = 0.5f;
+
+    [SerializeField] private Image mainImage;
+
+    [SerializeField] private RectTransform imageTransform;
+
+    public List<CutsceneData> scenes;
+
+    private Coroutine thisRoutine;
+
+    void Start()
+    {
+        thisRoutine = StartCoroutine(PlayCutscene());
+    }
+
+    public IEnumerator PlayCutscene()
+    {
+        yield return new WaitForSeconds(startDelay);
+
+        foreach(var data in scenes)
+        {
+            mainImage.sprite = data.sprite;
+            imageTransform.anchoredPosition = data.startOffset;
+            imageTransform.localScale = data.startScale;
+
+            // Animate transitions (myself, cause scale + move is broken)
+            imageTransform.DOAnchorPos(data.endOffset, data.time);
+            imageTransform.DOScale(data.endScale, data.time);
+
+
+            yield return new WaitForSeconds(data.time);
+        }
+
+        SkipCutscene();
+    }
+
+
+    public InputActionReference skipAction;
+
+    void OnEnable()
+    {
+        skipAction.action.performed += SkipAction;
+        skipAction.action.Enable();
+    }
+    void OnDisable()
+    {
+        skipAction.action.performed -= SkipAction;
+        skipAction.action.Disable();
+    }
+    
+    private void SkipAction(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("skipping cutscene");
+
+        SkipCutscene();
+    }
+
+    private void SkipCutscene()
+    {
+        StopCoroutine(thisRoutine);
+
+        GameManager.Instance.StartGame();
+    }
+
+
+
+    [Serializable]
+    public class CutsceneData
+    {
+        public Sprite sprite;
+        public Vector2 startOffset;
+        public Vector2 endOffset;
+        public float time;
+        public Vector3 startScale = Vector3.one;
+        public Vector3 endScale = Vector3.one;
+    }
+
+}
