8d1822b16c5fade7ebed4a05c6d22b1b55037408
Jan Hajciar <21070572@cuni.cz>
2026-06-05 13:44:40 +0200

    Bit improved energy particles

--- Assets/Scripts/Game/SFX/EnergyParticleMover.cs
+++ Assets/Scripts/Game/SFX/EnergyParticleMover.cs
@@ -17,19 +17,25 @@ public class EnergyParticleMover : MonoBehaviour
         StartCoroutine(Move(targetPosition));
     }
     private IEnumerator Move(Vector3 targetPosition) {
-        // Flip based on direction - TODOH actually rotate, not just flip
-        if (TryGetComponent<ParticleSystemRenderer>(out var psr)) {
-            var flip = psr.flip;
-            flip.x = targetPosition.x < transform.position.x ? 1 : 0;
-            psr.flip = flip;
+        // Flip based on direction - TODOH DOMove with MyTime
+        if (TryGetComponent<ParticleSystem>(out var psr)) {
+            var direction = (targetPosition - transform.position).normalized;
+            var angle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
+            //Debug.Log($"{direction} {angle}");
+            transform.localEulerAngles = transform.localEulerAngles.SetY(-angle);
+            //var main = psr.main;
+            //main.rotation = angle;
+            //var flip = psr.flip;
+            //flip.x = targetPosition.x < transform.position.x ? 1 : 0;
+            //psr.flip = flip;
         }
 
-        yield return new WaitForSeconds(delayOnStart);
+        yield return MyTime.WaitForSeconds(delayOnStart);
 
         float duration = (transform.position - targetPosition).magnitude / speed;
         transform.DOMove(targetPosition, (transform.position - targetPosition).magnitude / speed).SetEase(Ease.InOutCubic);
 
-        yield return new WaitForSeconds(duration + delayOnEnd);
+        yield return MyTime.WaitForSeconds(duration + delayOnEnd);
         gameObject.SmartDestroy();
     }
 }
