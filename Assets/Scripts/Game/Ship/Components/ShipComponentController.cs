f19dbf3ea53c53523cc8c80cff7747a8a9276fe2
Jakub Jankovec <jankovec.jakub@gmail.com>
2026-06-09 22:46:51 +0200

    Fix debug

--- Assets/Scripts/Game/Ship/Components/ShipComponentController.cs
+++ Assets/Scripts/Game/Ship/Components/ShipComponentController.cs
@@ -177,7 +177,7 @@ public class ShipComponentController : MonoBehaviour
 
         if (cooldown != null && !cooldown.IsReady && !GameManager.Instance.currentGameplayManager.tutorialRunning)
         {
-            Debug.Log("cooldown not ready");
+            // Debug.Log("cooldown not ready");
             return;
         }
 
