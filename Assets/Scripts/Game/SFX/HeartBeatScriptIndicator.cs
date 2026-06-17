3a279e2439de37c605fb943547aadfc670ef6efa
Jakub Jankovec <jankovec.jakub@gmail.com>
2026-06-17 13:16:42 +0200

    Big dino almost complete

--- Assets/Scripts/Game/SFX/HeartBeatScriptIndicator.cs
+++ Assets/Scripts/Game/SFX/HeartBeatScriptIndicator.cs
@@ -13,36 +13,9 @@ public class HeartBeatScriptIndicator : MonoBehaviour
     }
 
 
-    [SerializeField] private MeshRenderer meshMat;
-    [SerializeField] private SpriteRenderer selfMat;
-
     void Update()
     {
         float pulse = 1f + Mathf.Sin(MyTime.time * speed) * scaleAmount;
         transform.localScale = baseScale * pulse;
-
-
-        if (!meshMat.gameObject.scene.IsValid())
-        {
-            return; // it's a prefab asset
-        }
-
-        // Copy mesh material damage value to the sprite
-        if (meshMat != null &&
-            meshMat.materials != null &&
-            meshMat.materials.Length > 0 &&
-            meshMat.materials[0] != null)
-        {
-            float dam = meshMat.materials[0].GetFloat("_damageAmount");
-            selfMat.material.SetFloat("_damageAmount", dam);
-
-            // broke the sprite, stop moving
-            if(dam >= 1f)
-            {
-                selfMat.color = Color.gray;
-                scaleAmount = 0f;
-            }
-        }
-
     }
 }
